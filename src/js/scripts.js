2b512783cd466b6943a3f36d66f51f1070292beb
itsmoongoose <lewishlemay@gmail.com>
2025-06-06 20:10:27 -0600

    Add main function and get password function

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,24 @@
+//Defining variables
+    const WrongPass = "Incorrect password. Please try again: ";
+    const WrongPassLast = "Incorrect password. This is your last attempt: ";
+    const ChangePass = "If you have forgotten your password, enter your username to see available options: "
+
+
+async function GetPass1() {
+
+//Function starts
+    let Password = await input("Please enter your password: ");
+    Password = await input(WrongPass);
+    Password = await input(WrongPassLast);
+    Password = await input(ChangePass);
+}
+
+
+
+
+
+
+
 async function main() {
 
 
@@ -5,9 +26,12 @@ async function main() {
     const UserList = [ "Shrek2onDVD", "makeawishkid", "real_name_hidden", "AllGoodNamesRGone",
         "test_name_please_ignore", "Babushka", "not.a.robot", "in_jail_out_soon", "afk",
         "LiveLaughToasterBath", "bulky_refrigerator", "blank" ];
-    let randomNum = Math.random() * 12;
+    let randomNum = Math.round(Math.random() * 12);
+
+//Function starts
+    output ("Welcome Back " + UserList[randomNum] + "!");
+    
 
 
 
-    output ("Welcome Back " + UserList[randomNum]);
 }
