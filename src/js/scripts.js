bd08f5fabf0b3d62b455a32436c639b7e122b43f
itsmoongoose <lewishlemay@gmail.com>
2025-06-06 21:25:34 -0600

    Finish menu options and start change password

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,8 @@
 //Defining variables
     const WrongPass = "Incorrect password. Please try again: ";
     const WrongPassLast = "Incorrect password. This is your last attempt: ";
-    const ChangePass = "If you have forgotten your password, enter your username to see available options: "
+    const ChangePass = "If you have forgotten your password, enter your username exactly as you see it to see available options: "
+    let Tries = 0;
 
 //Function -- get password
 
@@ -39,22 +40,33 @@ async function main() {
     Password = await input(ChangePass);
 
 //Menu Options
-    output("Please enter an option from the list below:");
     let MenuOptions = [ "1. Reset Password" ];
+    output("Please enter an option from the list below:");
+    output(MenuOptions);
     let MenuChoice = await input("");
     MenuChoice = MenuChoice.toLowerCase();
-    if (MenuChoice == "1" || MenuChoice.includes("reset")) {
-        
-    } else {
-        while (MenuChoice) {
-            output("Your input is not recognized. Please try again.")
+
+    //User input doesn't match menu options
+    while (!MenuChoice == "1" || !MenuChoice.includes("reset")) {
+        output("Your input is not recognized. Please try again.");
+        let Tries = (Tries + 1);
+
+        //Too many tries -- program terminates
+        if (Tries >= 3) {
+            output("Unrecognized input limit reached. This connection may have been compromised; the program will terminate immediately and all your data will be destroyed.");
+            break;
+        } else {
+            MenuChoice = await input(" ");
         }
-    }
-  
-
-    }
     
+    //Change Password -- answer security question
+    } if (MenuChoice == "1" || MenuChoice.includes("reset")) {
+        output("To change your password, please verify your identity by entering your email address or choosing to answer your self-created security question with 'Q'");
+
+    }
 
 
 
-}
+
+
+}  
